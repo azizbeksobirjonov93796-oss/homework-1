e898d1f0948099630192ab5f348a642a4bb09813
Azizbek Sobirjonov <azizbeksobirjonov93796@gmail.com>
2026-07-15 21:06:15 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+# 1-Topshiriq: Car
+
+class Car:
+    markasi = ""
+    modeli = ""
+    yili = ""
+
+car1 = Car()
+car1.markasi = "Chevrolet"
+car1.modeli = "Cobalt"
+car1.yili = 2022
+
+car2 = Car()
+car2.markasi = "Kia"
+car2.modeli = "K5"
+car2.yili = 2024
+
+print("1-mashina")
+print(car1.markasi)
+print(car1.modeli)
+print(car1.yili)
+
+print()
+
+print("2-mashina")
+print(car2.markasi)
+print(car2.modeli)
+print(car2.yili)
+
+
+# 2-Topshiriq: Student
+
+class oquvchi:
+    ism = ""
+    yosh = 0
+    sinf = 0
+
+oquvchi1 = oquvchi()
+oquvchi1.ism = "Aziz"
+oquvchi1.yosh = 14
+oquvchi1.sinf = 8
+
+print("\n1-oquvchi")
+print(oquvchi1.ism)
+print(oquvchi1.yosh)
+print(oquvchi1 .sinf)
+
+print()
+# 3-Topshiriq: Book
+
+class Book:
+    nomi = ""
+    muallifi = ""
+    sahifa = 0
+
+book1 = Book()
+book1.nomi = "Otkan kunlar"
+book1.muallifi = "Abdulla Qodiriy"
+book1.sahifa = 400
+
+book2 = Book()
+book2.nomi = "Mehrobdan Chayon"
+book2.muallifi = "Abdulla Qodiriy"
+book2.sahifa = 350
+
+print("\n1 kitob")
+print(book1.nomi)
+print(book1.muallifi)
+print(book1.sahifa)
+
+print()
+
+print("2 kitob")
+print(book2.nomi)
+print(book2.muallifi)
+print(book2.sahifa)
